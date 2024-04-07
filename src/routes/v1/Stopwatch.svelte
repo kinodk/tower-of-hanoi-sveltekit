af35691f304b4d539f5cf38653f52b8569c1dc0c
Kino Ko <kinodenzelko@gmail.com>
2024-04-07 16:17:40 +0800

    feat: added stopwatch that works on keydown but does not yet stop or reset. (also no styling yet)

---
+++ src/routes/v1/Stopwatch.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let startTime: number;
+	let elapsedTime: number;
+
+	$: hours = String(Math.floor(elapsedTime / 1000 / 60 / 60)).padStart(2, '0') + ':';
+	$: minutes = String(Math.floor(elapsedTime / 1000 / 60) % 60).padStart(2, '0') + ':';
+	$: seconds = String(Math.floor((elapsedTime / 1000) % 60)).padStart(2, '0') + '.';
+	$: millis = String(Math.floor((elapsedTime % 1000) / 10)).padStart(2, '0');
+	$: formattedElapsedTime = `${hours == 'NaN:' || hours == '00:' ? '' : hours}${minutes == 'NaN:' || minutes == '00:' ? '00:' : minutes}${seconds == 'NaN.' || seconds == '00.' ? '00.' : seconds}${millis == 'NaN' ? '00' : millis}`;
+
+	const startStopwatch = () => {
+		startTime = Date.now();
+		setInterval(() => {
+			let endTime = Date.now();
+			elapsedTime = endTime - startTime;
+		});
+	};
+</script>
+
+<svelte:document on:keydown|once={startStopwatch} />
+
+{formattedElapsedTime}
